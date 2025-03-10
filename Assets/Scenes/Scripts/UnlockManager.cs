e321faf6dce59cb8bffcd5529d9265cc94477f1e
SEONG WON CHOI <qwik0102@gmail.com>
2025-03-11 00:18:43 +0900

    [feat] UnlockManager 스크립트 추가, 아직 미완임

--- Assets/Scenes/Scripts/UnlockManager.cs
+++ Assets/Scenes/Scripts/UnlockManager.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 
 public class UnlockManager : MonoBehaviour
 {
     public static UnlockManager instance;
+
+    public event Action OnUnlock;
 
     [SerializeField] private GameObject _baseButtonParent;
     [SerializeField] private GameObject _cookButtonParent;
@@ -17,23 +20,41 @@ public class UnlockManager : MonoBehaviour
 
     public void Unlock(Ingredient.Base baseIngred)
     {
-
+        PlayerPrefs.SetInt(baseIngred.ToString(), 1);
     }
 
     public void Unlock(Ingredient.Cook cook)
     {
-
+        PlayerPrefs.SetInt(cook.ToString(), 1);
     }
 
     public void Unlock(Ingredient.MeatFish meatFish)
     {
-
+        PlayerPrefs.SetInt(meatFish.ToString(), 1);
     }
 
     public void Unlock(Ingredient.Vege vege)
     {
-
+        PlayerPrefs.SetInt(vege.ToString(), 1);
     }
 
+    public bool IsUnlocked(Ingredient.Base baseIngred)
+    {
+        return PlayerPrefs.HasKey(baseIngred.ToString()) && PlayerPrefs.GetInt(baseIngred.ToString()) == 1;
+    }
 
+    public bool IsUnlocked(Ingredient.Cook cook)
+    {
+        return PlayerPrefs.HasKey(cook.ToString()) && PlayerPrefs.GetInt(cook.ToString()) == 1;
+    }
+
+    public bool IsUnlocked(Ingredient.MeatFish meatFish)
+    {
+        return PlayerPrefs.HasKey(meatFish.ToString()) && PlayerPrefs.GetInt(meatFish.ToString()) == 1;
+    }
+
+    public bool IsUnlocked(Ingredient.Vege vege)
+    {
+        return PlayerPrefs.HasKey(vege.ToString()) && PlayerPrefs.GetInt(vege.ToString()) == 1;
+    }
 }
