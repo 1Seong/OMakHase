dc8af7e55f56401785254021d8d4f63fb56259ed
SEONG WON CHOI <qwik0102@gmail.com>
2025-03-18 18:29:27 +0900

    [feat]: UnlockManager 추가 작성(미완성)

--- Assets/Scenes/Scripts/UnlockManager.cs
+++ Assets/Scenes/Scripts/UnlockManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnlockManager : MonoBehaviour
 {
@@ -7,54 +8,99 @@ public class UnlockManager : MonoBehaviour
 
     public event Action OnUnlock;
 
+    // 아래 버튼들 따로 컴포넌트에 분리해서 버튼 부모에 직접 부착하기
     [SerializeField] private GameObject _baseButtonParent;
     [SerializeField] private GameObject _cookButtonParent;
     [SerializeField] private GameObject _meatFishParent;
     [SerializeField] private GameObject _vegeParent;
 
+    private Button[] _baseButtons; // Last button is 'next' button
+    private Button[] _cookButtons; // Last two buttons are buttons that change screen
+    private Button[] _meatFishButtons;
+    private Button[] _vegeButtons;
+
     private void Awake()
     {
         instance = this;
         DontDestroyOnLoad(this);
+
+        _baseButtons = _baseButtonParent.GetComponentsInChildren<Button>();
+        _cookButtons = _cookButtonParent.GetComponentsInChildren<Button>();
+        _meatFishButtons = _meatFishParent.GetComponentsInChildren <Button>();
+        _vegeButtons = _vegeParent.GetComponentsInChildren<Button>();
+    }
+
+    // Add this method to game start(Load game) button 
+    public void InitButtons()
+    {
+        foreach (Ingredient.Base i in Enum.GetValues(typeof(Ingredient.Base)))
+            if (IsUnlocked(i))
+                OnUnlock.Invoke();
+
+        foreach (Ingredient.Cook i in Enum.GetValues(typeof(Ingredient.Cook)))
+            if (IsUnlocked(i))
+                OnUnlock.Invoke();
+
+        foreach (Ingredient.MeatFish i in Enum.GetValues(typeof(Ingredient.MeatFish)))
+            if (IsUnlocked(i))
+                OnUnlock.Invoke();
+
+        foreach (Ingredient.Vege i in Enum.GetValues(typeof(Ingredient.Vege)))
+            if (IsUnlocked(i))
+                OnUnlock.Invoke();
+    }
+
+    public void ResetData()
+    {
+        PlayerPrefs.DeleteAll();
     }
 
     public void Unlock(Ingredient.Base baseIngred)
     {
         PlayerPrefs.SetInt(baseIngred.ToString(), 1);
+        OnUnlock.Invoke();
     }
 
     public void Unlock(Ingredient.Cook cook)
     {
         PlayerPrefs.SetInt(cook.ToString(), 1);
+        OnUnlock.Invoke();
     }
 
     public void Unlock(Ingredient.MeatFish meatFish)
     {
         PlayerPrefs.SetInt(meatFish.ToString(), 1);
+        OnUnlock.Invoke();
     }
 
     public void Unlock(Ingredient.Vege vege)
     {
         PlayerPrefs.SetInt(vege.ToString(), 1);
+        OnUnlock.Invoke();
     }
 
-    public bool IsUnlocked(Ingredient.Base baseIngred)
+    private void UnlockButton(Ingredient.Base baseIngred)
     {
-        return PlayerPrefs.HasKey(baseIngred.ToString()) && PlayerPrefs.GetInt(baseIngred.ToString()) == 1;
+        _baseButtons[(int)baseIngred].gameObject.SetActive(true);
     }
 
-    public bool IsUnlocked(Ingredient.Cook cook)
+    private void UnlockButton(Ingredient.Cook cook)
     {
-        return PlayerPrefs.HasKey(cook.ToString()) && PlayerPrefs.GetInt(cook.ToString()) == 1;
+        _cookButtons[(int)cook].gameObject.SetActive(true);
     }
 
-    public bool IsUnlocked(Ingredient.MeatFish meatFish)
+    private void UnlockButton(Ingredient.MeatFish meatFish)
     {
-        return PlayerPrefs.HasKey(meatFish.ToString()) && PlayerPrefs.GetInt(meatFish.ToString()) == 1;
+        _meatFishButtons[(int)meatFish].gameObject.SetActive(true);
     }
 
-    public bool IsUnlocked(Ingredient.Vege vege)
+    private void UnlockButton(Ingredient.Vege vege)
     {
-        return PlayerPrefs.HasKey(vege.ToString()) && PlayerPrefs.GetInt(vege.ToString()) == 1;
+        _cookButtons[(int)vege].gameObject.SetActive(true);
+    }
+
+    private bool IsUnlocked<T>(T item)
+    {
+        return PlayerPrefs.HasKey(item.ToString()) && PlayerPrefs.GetInt(item.ToString()) == 1;
     }
 }
